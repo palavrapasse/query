3a7dc00f5016b37ffc2d3a435007f4d2224b35fc
freitzzz <1160907@isep.ipp.pt>
2023-01-02 21:52:29 +0000

    include main program to start query service

---
+++ cmd/query/query.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/palavrapasse/query/internal/data"
+	"github.com/palavrapasse/query/internal/http"
+)
+
+func main() {
+
+	e := echo.New()
+
+	defer e.Close()
+
+	dbctx, oerr := data.Open()
+
+	if oerr != nil {
+
+		log.Printf("Could not open DB connection on server start")
+		log.Printf(oerr.Error())
+
+		panic("DB connection is required to operate query")
+	}
+
+	http.RegisterMiddlewares(e, dbctx)
+	http.RegisterHandlers(e)
+
+	e.Logger.Fatal(http.Start(e))
+}
