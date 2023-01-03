02dba918044d1ddeef775e4db1a07a05740834bc
freitzzz <1160907@isep.ipp.pt>
2023-01-03 21:40:58 +0000

    include parse of affacted query param and converter to hash user functions

--- internal/http/param.go
+++ internal/http/param.go
@@ -1,5 +1,11 @@
 package http
 
+import (
+	"strings"
+
+	"github.com/palavrapasse/damn/pkg/entity"
+)
+
 const (
 	leakTypeQueryParam = "type"
 	affectedQueryParam = "affected"
@@ -46,4 +52,19 @@ func ParseTarget(s string) Target {
 	}
 
 	return t
+}
+
+func ParseAffected(s string) []string {
+	return strings.Split(strings.ReplaceAll(s, " ", ""), ",")
+}
+
+func AffectedToHashUser(aff []string) []entity.HashUser {
+	laff := len(aff)
+	hus := make([]entity.HashUser, laff)
+
+	for i := 0; i < laff; i++ {
+		hus[i] = entity.NewHashUser(entity.User{Email: entity.Email(aff[i])})
+	}
+
+	return hus
 }
