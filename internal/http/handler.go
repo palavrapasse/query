e5d85756ca88a90eaf5b6854220b4e6cb817f3e1
freitzzz <1160907@isep.ipp.pt>
2023-01-03 21:42:25 +0000

    parse affected query param and then query leaks in database

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -1,10 +1,11 @@
 package http
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/palavrapasse/damn/pkg/entity"
+	"github.com/palavrapasse/query/internal/data"
 )
 
 func RegisterHandlers(e *echo.Echo) {
@@ -16,13 +17,25 @@ func RegisterHandlers(e *echo.Echo) {
 
 func QueryLeaks(ectx echo.Context) error {
 
-	_, gmerr := GetMiddlewareContext(ectx)
+	mwctx, gmerr := GetMiddlewareContext(ectx)
 
 	if gmerr != nil {
 		return InternalServerError(ectx)
 	}
 
-	return Ok(ectx, ToQueryLeaksView([]entity.Leak{}))
+	affp := ectx.QueryParam(affectedQueryParam)
+	aff := ParseAffected(affp)
+	hus := AffectedToHashUser(aff)
+
+	ls, err := data.QueryLeaksDB(mwctx.DB, hus)
+
+	if err != nil {
+		log.Printf("wtf happened: %v\n", err)
+
+		return InternalServerError(ectx)
+	}
+
+	return Ok(ectx, ToQueryLeaksView(ls))
 }
 
 func useNotFoundHandler() func(c echo.Context) error {
