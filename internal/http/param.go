9b38cf708eb8b0a2b17a7502e8f0cadc21518ad3
freitzzz <1160907@isep.ipp.pt>
2023-01-08 12:26:28 +0000

    remove unused functions

--- internal/http/param.go
+++ internal/http/param.go
@@ -27,33 +27,6 @@ const (
 type LeakType string
 type Target string
 
-func ParseLeakType(s string) LeakType {
-	lt := LeakType(s)
-
-	switch lt {
-	case EmailLeakType:
-	case PasswordLeakType:
-	default:
-		lt = UnknownLeakType
-	}
-
-	return lt
-}
-
-func ParseTarget(s string) Target {
-	t := Target(s)
-
-	switch t {
-	case AllTarget:
-	case NewestTarget:
-	case OldestTarget:
-	default:
-		t = AllTarget
-	}
-
-	return t
-}
-
 func ParseAffected(s string) []string {
 	return strings.Split(strings.ReplaceAll(s, " ", ""), ",")
 }
