d0201fa5660b14da117b906bc7e37955b59e96b9
freitzzz <1160907@isep.ipp.pt>
2023-01-11 22:07:33 +0000

    implement platforms most recent/oldest affected query

--- internal/data/query.go
+++ internal/data/query.go
@@ -24,7 +24,14 @@ SELECT L.* FROM Leak L
 ORDER BY L.sharedatesc %s
 `
 
-const platformsQuery = `SELECT * FROM Platform`
+const platformsQuery = `SELECT P.name FROM Platform P`
+
+const platformsByLeaksQuery = `
+SELECT P.name FROM LeakPlatform LP
+INNER JOIN Leak L ON L.leakid == LP.leakid
+INNER JOIN Platform P on P.platid == LP.platid
+ORDER BY L.sharedatesc %s
+`
 
 var leaksByUserQueryMapper = func() (*QueryLeaksResult, []any) {
 	aul := QueryLeaksResult{}
@@ -41,7 +48,7 @@ var leaksQueryMapper = func() (*QueryLeaksResult, []any) {
 var platformsQueryMapper = func() (*QueryPlatformsResult, []any) {
 	qpr := QueryPlatformsResult{}
 
-	return &qpr, []any{&qpr.PlatId, &qpr.Name}
+	return &qpr, []any{&qpr.Name}
 }
 
 func QueryLeaksDB(dbctx database.DatabaseContext[database.Record], tt Target, hus ...entity.HashUser) ([]QueryLeaksResult, error) {
@@ -57,7 +64,11 @@ func QueryLeaksDB(dbctx database.DatabaseContext[database.Record], tt Target, hu
 func QueryPlaformsDB(dbctx database.DatabaseContext[database.Record], tt Target) ([]QueryPlatformsResult, error) {
 	ctx := database.Convert[database.Record, QueryPlatformsResult](dbctx)
 
-	return queryPlatforms(ctx, tt)
+	if tt == AllTarget {
+		return queryPlatforms(ctx)
+	}
+
+	return queryPlatformsByLeaks(ctx, tt)
 }
 
 func queryLeaksThatAffectUser(dbctx database.DatabaseContext[QueryLeaksResult], hus []entity.HashUser) ([]QueryLeaksResult, error) {
@@ -72,8 +83,14 @@ func queryLeaks(dbctx database.DatabaseContext[QueryLeaksResult], tt Target) ([]
 	return dbctx.CustomQuery(q, m, vs...)
 }
 
-func queryPlatforms(dbctx database.DatabaseContext[QueryPlatformsResult], tt Target) ([]QueryPlatformsResult, error) {
-	q, m, vs := preparePlatformsQuery(tt)
+func queryPlatforms(dbctx database.DatabaseContext[QueryPlatformsResult]) ([]QueryPlatformsResult, error) {
+	q, m, vs := preparePlatformsQuery()
+
+	return dbctx.CustomQuery(q, m, vs...)
+}
+
+func queryPlatformsByLeaks(dbctx database.DatabaseContext[QueryPlatformsResult], tt Target) ([]QueryPlatformsResult, error) {
+	q, m, vs := preparePlatformsByLeaksQuery(tt)
 
 	return dbctx.CustomQuery(q, m, vs...)
 }
@@ -94,6 +111,10 @@ func prepareLeaksQuery(tt Target) (string, database.TypedQueryResultMapper[Query
 	return fmt.Sprintf(leaksQuery, tt.ToSQLKeyword()), leaksQueryMapper, []any{}
 }
 
-func preparePlatformsQuery(tt Target) (string, database.TypedQueryResultMapper[QueryPlatformsResult], []any) {
+func preparePlatformsQuery() (string, database.TypedQueryResultMapper[QueryPlatformsResult], []any) {
 	return platformsQuery, platformsQueryMapper, []any{}
+}
+
+func preparePlatformsByLeaksQuery(tt Target) (string, database.TypedQueryResultMapper[QueryPlatformsResult], []any) {
+	return fmt.Sprintf(platformsByLeaksQuery, tt.ToSQLKeyword()), platformsQueryMapper, []any{}
 }
